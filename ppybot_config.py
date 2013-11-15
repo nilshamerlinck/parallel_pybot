43bf137a7eba30324614c080da281429caaebf61
Bruno Bossola <bbossola@gmail.com>
2013-11-15 18:43:46 +0000

    Fixed windows CI config

--- ppybot_config.py
+++ ppybot_config.py
@@ -1,3 +1,8 @@
-PYBOT_CMD = "/usr/local/bin/pybot"
+# Linux configuration
+# PYBOT_CMD = "/usr/local/bin/pybot"
+
+# Windows CI configuration
+PYBOT_CMD = "c:\\Python27\\Scripts\\pybot.bat"
+
 MAX_PARALLEL_TESTS = 5
 MAX_EXECUTION_TIME = 300
